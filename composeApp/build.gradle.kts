6e4ccade9e8a6e2fb7effbd12fac1f7723f6cc22
Alex Krantz <alex@krantz.dev>
2025-11-06 10:58:30 -0800

    Add basic camera control

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -40,6 +40,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(projects.shared)
+            implementation(libs.camerak)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
--- composeApp/src/commonMain/kotlin/you/yearof/app/App.kt
+++ composeApp/src/commonMain/kotlin/you/yearof/app/App.kt
@@ -1,48 +1,111 @@
 package you.yearof.app
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import app.composeapp.generated.resources.Res
-import app.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
+import com.kashif.cameraK.controller.CameraController
+import com.kashif.cameraK.enums.*
+import com.kashif.cameraK.permissions.providePermissions
+import com.kashif.cameraK.ui.CameraPreview
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
-@Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        val greeting = remember { Greeting().greet() }
         Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
+            modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer).safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+            Text("Compose: $greeting")
+            Camera()
+        }
+    }
+}
+
+@Composable
+fun Camera() {
+    val permissions = providePermissions()
+    var cameraPermissionState by remember { mutableStateOf(permissions.hasCameraPermission()) }
+
+    if (cameraPermissionState) {
+        ShowCamera()
+    } else {
+        permissions.RequestCameraPermission(
+            onGranted = { cameraPermissionState = true },
+            onDenied = { println("Camera permission denied") })
+    }
+}
+
+@Composable
+fun ShowCamera() {
+    val scope = rememberCoroutineScope()
+
+    var cameraController by remember { mutableStateOf<CameraController?>(null) }
+
+    var overlayAngle by remember { mutableFloatStateOf(0f) }
+    val rotation by animateFloatAsState(
+        targetValue = overlayAngle,
+        animationSpec = tween(
+            durationMillis = 600,
+            easing = FastOutSlowInEasing,
+        ),
+        label = "cameraFlip"
+    )
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer {
+                    rotationY = rotation
+                    cameraDistance = 32 * density
+                },
+        ) {
+            CameraPreview(
+                modifier = Modifier.fillMaxSize(),
+                cameraConfiguration = {
+                    setCameraLens(CameraLens.BACK)
+                    setFlashMode(FlashMode.AUTO)
+                    setImageFormat(ImageFormat.JPEG)
+                    setDirectory(Directory.PICTURES)
+                    setTorchMode(TorchMode.OFF)
+                    setQualityPrioritization(QualityPrioritization.QUALITY)
+                },
+                onCameraControllerReady = { cameraController = it },
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 32.dp)
+                .align(Alignment.BottomCenter),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Button(onClick = {
+                cameraController?.let { controller ->
+                    scope.launch {
+                        overlayAngle += 180f
+
+                        delay(300)
+                        controller.toggleCameraLens()
+                    }
                 }
+            }) {
+                Text("Switch Camera")
             }
         }
     }
