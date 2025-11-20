3d79734ed63ea2219aaa5b30b60db2ade9ba6920
Alex Krantz <alex@krantz.dev>
2025-11-20 10:23:55 -0800

    Align ios camera preview with android

--- composeApp/src/iosMain/kotlin/you/yearof/app/camera/CameraPreview.ios.kt
+++ composeApp/src/iosMain/kotlin/you/yearof/app/camera/CameraPreview.ios.kt
@@ -1,12 +1,17 @@
 package you.yearof.app.camera
 
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.UIKitView
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.readValue
 import platform.AVFoundation.AVCaptureVideoPreviewLayer
-import platform.AVFoundation.AVLayerVideoGravityResizeAspectFill
+import platform.AVFoundation.AVLayerVideoGravityResizeAspect
 import platform.CoreGraphics.CGRectZero
 import platform.UIKit.UIView
 
@@ -16,22 +21,35 @@ actual fun CameraPreview(
     controller: CameraController,
     modifier: Modifier,
 ) {
-    UIKitView(
+    BoxWithConstraints(
         modifier = modifier,
-        factory = {
-            val previewLayer =
-                AVCaptureVideoPreviewLayer(session = controller.camera.session).apply {
-                    videoGravity = AVLayerVideoGravityResizeAspectFill
-                }
-
-            object : UIView(frame = CGRectZero.readValue()) {
-                override fun layoutSubviews() {
-                    super.layoutSubviews()
-                    previewLayer.frame = bounds
-                }
-            }.apply {
-                layer.addSublayer(previewLayer)
+        contentAlignment = Alignment.Center,
+    ) {
+        val previewAspectRatio = 3f / 4f
+        val previewModifier =
+            if (maxHeight * previewAspectRatio <= maxWidth) {
+                Modifier.fillMaxHeight().aspectRatio(previewAspectRatio)
+            } else {
+                Modifier.fillMaxWidth().aspectRatio(previewAspectRatio)
             }
-        },
-    )
+
+        UIKitView(
+            modifier = previewModifier,
+            factory = {
+                val previewLayer =
+                    AVCaptureVideoPreviewLayer(session = controller.camera.session).apply {
+                        videoGravity = AVLayerVideoGravityResizeAspect
+                    }
+
+                object : UIView(frame = CGRectZero.readValue()) {
+                    override fun layoutSubviews() {
+                        super.layoutSubviews()
+                        previewLayer.frame = bounds
+                    }
+                }.apply {
+                    layer.addSublayer(previewLayer)
+                }
+            },
+        )
+    }
 }
