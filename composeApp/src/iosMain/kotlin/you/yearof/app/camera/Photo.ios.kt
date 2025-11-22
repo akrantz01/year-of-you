f95183791d99c5ef5b0a3d0723c99588e3c471a1
Alex Krantz <alex@krantz.dev>
2025-11-21 19:50:22 -0800

    Remove unused photo abstraction

--- composeApp/src/iosMain/kotlin/you/yearof/app/camera/Photo.ios.kt
+++
@@ -1,7 +0,0 @@
-package you.yearof.app.camera
-
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.toComposeImageBitmap
-import org.jetbrains.skia.Image
-
-actual fun Photo.toImageBitmap(): ImageBitmap = Image.makeFromEncoded(this).toComposeImageBitmap()
