363b79628d7707693261577fda90230e0d32c896
Alex Krantz <alex@krantz.dev>
2025-12-04 18:40:52 -0800

    Fix ios camera preview

--- composeApp/src/commonMain/kotlin/you/yearof/app/camera/CameraPreview.kt
+++ composeApp/src/commonMain/kotlin/you/yearof/app/camera/CameraPreview.kt
@@ -1,13 +1,11 @@
 package you.yearof.app.camera
 
-import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
 
 @Composable
 expect fun CameraPreview(
@@ -22,17 +20,10 @@ internal fun PreviewAspectRatio(
     modifier: Modifier = Modifier,
     composable: @Composable (modifier: Modifier) -> Unit,
 ) {
-    BoxWithConstraints(
+    Box(
         modifier = modifier,
         contentAlignment = Alignment.Center,
     ) {
-        val aspectRatioModifier =
-            if (maxHeight * PreviewAspectRatio <= maxWidth) {
-                Modifier.fillMaxHeight().aspectRatio(PreviewAspectRatio)
-            } else {
-                Modifier.fillMaxWidth().aspectRatio(PreviewAspectRatio)
-            }
-
-        composable(aspectRatioModifier)
+        composable(modifier.fillMaxWidth().aspectRatio(PreviewAspectRatio))
     }
 }
--- composeApp/src/iosMain/kotlin/you/yearof/app/camera/CameraPreview.ios.kt
+++ composeApp/src/iosMain/kotlin/you/yearof/app/camera/CameraPreview.ios.kt
@@ -4,10 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.UIKitView
 import kotlinx.cinterop.readValue
+import platform.AVFoundation.AVCaptureVideoOrientationPortrait
 import platform.AVFoundation.AVCaptureVideoPreviewLayer
 import platform.AVFoundation.AVLayerVideoGravityResizeAspect
 import platform.CoreGraphics.CGRectZero
 import platform.UIKit.UIView
+import platform.UIKit.UIViewMeta
 
 @Composable
 actual fun CameraPreview(
@@ -18,20 +20,24 @@ actual fun CameraPreview(
         UIKitView(
             modifier = inner,
             factory = {
-                val previewLayer =
-                    AVCaptureVideoPreviewLayer(session = controller.camera.session).apply {
-                        videoGravity = AVLayerVideoGravityResizeAspect
-                    }
+                PreviewView().apply {
+                    clipsToBounds = true
+                    layer.cornerRadius = 16.0
 
-                object : UIView(frame = CGRectZero.readValue()) {
-                    override fun layoutSubviews() {
-                        super.layoutSubviews()
-                        previewLayer.frame = bounds
-                    }
-                }.apply {
-                    layer.addSublayer(previewLayer)
+                    previewLayer.session = controller.camera.session
+                    previewLayer.videoGravity = AVLayerVideoGravityResizeAspect
+                    previewLayer.connection?.videoOrientation = AVCaptureVideoOrientationPortrait
                 }
             },
         )
+    }
+}
+
+private class PreviewView : UIView(frame = CGRectZero.readValue()) {
+    val previewLayer: AVCaptureVideoPreviewLayer
+        get() = layer as AVCaptureVideoPreviewLayer
+
+    companion object : UIViewMeta() {
+        override fun layerClass() = AVCaptureVideoPreviewLayer
     }
 }
