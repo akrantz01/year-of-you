fa328d03df61c5749eb5b1d4c0b08a68ed098f84
Alex Krantz <alex@krantz.dev>
2025-12-04 20:25:58 -0800

    Add splash screen for ios

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -5,6 +5,9 @@ import ComposeApp
 struct iOSApp: App {
     init() {
         KoinKt.startKoin()
+        
+        // hacky but forces the launch screen to show so ¯\_(ツ)_/¯
+        usleep(useconds_t(300 * 1000))
     }
 
     var body: some Scene {
